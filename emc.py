1c5c3ed524afcbc84c8312f0bca321afac924d6c
Dmitry Ponyatov <dponyatov@gmail.com>
2019-03-03 12:47:37 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ emc.py
@@ -0,0 +1,13 @@
+## @file
+## @brief @ref emc
+## @copyright Dmitry Ponyatov <dponyatov@gmail.com> CC-NC-ND
+## github: https://github.com/ponyatov/metapy430
+
+## @defgroup emc embedded C
+## @brief @ref emc
+## @ingroup lang 
+## @{
+
+
+
+## @}
